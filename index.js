f341c64c4b6511d57f4691fd8486c5b2cd8e4d0d
Lili Molloy <liliana.molloy@gmail.com>
2018-12-06 16:07:15 +1100

    channged css colours, added js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const inputs = document.querySelectorAll('.controls input') //selects aall the inputs on the page
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || ''; //bacause the colour does not have sizing
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    // console.log(this.name)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
